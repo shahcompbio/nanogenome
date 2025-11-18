d817159f5ca1cd9e049c1533d8e22a2f856092b1
apsteinberg <asherps@gmail.com>
2025-11-18 18:32:13 -0500

    update to karyoplot

--- bin/svkaryoplot.R
+++ bin/svkaryoplot.R
@@ -13,18 +13,19 @@ dd.filtered <- dd %>% filter(chrom1 == chrom2)
 sv.data <- toGRanges(dd.filtered[,c("chrom1", "base1", "base2", "SV_Type")])
 # Assign colors based on SV type
 sv.colors <- ifelse(dd.filtered$SV_Type == "INS", "#CC6677",
-                    ifelse(dd.filtered$SV_Type == "DEL", "#332288", "#DDDDDD"))
+                    ifelse(dd.filtered$SV_Type == "DEL", "#332288", "#CCBB44"))
 
 # plot
 svg(file=out.file, height=11, width=8);
 # Create karyotype plot
-kp <- plotKaryotype(genome=genome.version)
-kpPlotRegions(kp, data=sv.data, col=sv.colors, r0=0, r1=2)
+kp <- plotKaryotype(genome=genome.version, plot.type = 2)
+kpPlotDensity(kp, data=sv.data)
+kpPlotRegions(kp, data=sv.data, col=sv.colors, data.panel = 2)
 legend("bottomright",
-        legend=c("Insertion", "Deletion", "Other"),
-        fill = c("#CC6677", "#332288", "#DDDDDD"),
-        title.adj = 0.2,
-        title = "SV Types"
-        )
+       legend=c("Insertion", "Deletion", "Other"),
+       fill = c("#CC6677", "#332288", "#CCBB44"),
+       title.adj = 0.2,
+       title = "SV Types"
+)
 # Close the SVG graphics device
 dev.off()
